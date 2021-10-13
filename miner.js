86f1f06789457db6ee6c8baddccd6417ddd17786
Alina <48862818+mapryl@users.noreply.github.com>
2021-10-13 23:03:27 +0300

    complete rewrite for new aw version fall 2021

--- miner.js
+++ miner.js
@@ -72,6 +72,7 @@ var miner_init_ui = function () {
             display: flex;
             flex-direction: column-reverse;
             align-items: flex-start;
+            z-index: 1000;
         }
 
         .miner-statusbar {
@@ -151,8 +152,8 @@ var miner_init_ui = function () {
   ui.classList.add("miner-ui-container");
 
   // append container over webgl viewport
-  const webgl_content = document.querySelector(".webgl-content");
-  webgl_content.appendChild(ui);
+  const root_item = document.querySelector("#root");
+  root_item.appendChild(ui);
 
   // create status bar
   const statusbar = document.createElement("div");
@@ -160,27 +161,6 @@ var miner_init_ui = function () {
   statusbar.textContent = "alien-worlds-miner";
   ui.appendChild(statusbar);
   miner.ui.statusbar = statusbar;
-
-  // create bag bar
-  const bag = document.createElement("div");
-  bag.classList.add("miner-bag");
-  bag.textContent = "bag: click to refresh";
-  ui.appendChild(bag);
-  miner.ui.bag = bag;
-
-  bag.addEventListener("click", async function () {
-    const ACCOUNT = wax.userAccount;
-    const items = await my_getBag(ACCOUNT);
-    let bag_str = "bag: [";
-
-    for (item of items) {
-      bag_str += item.asset_id + ", "
-    }
-    bag_str = bag_str.slice(0, -2);
-
-    bag_str += "] click to refresh";
-    bag.textContent = bag_str;
-  });
 
   // create the Mining button
   const button = document.createElement("button");
@@ -201,56 +181,6 @@ var miner_init_ui = function () {
       my_mine_loop();
     }
   });
-
-  // Claim nft button
-  const claimnfts = document.createElement("button");
-  claimnfts.classList.add("miner-claimnfts");
-  claimnfts.classList.add("miner-hidden");
-
-  claimnfts.textContent = "claim nfts";
-
-  ui.appendChild(claimnfts);
-  miner.ui.claimnfts = claimnfts;
-
-  // 3. Add event handler
-  claimnfts.addEventListener("click", function () {
-    try_claimnft();
-  });
-
-  update_unclaimed_nft_count();
-}
-
-async function update_unclaimed_nft_count() {
-  // const now = Date.now();
-  // const last_claim = localStorage.getItem("last_nft_claim_date");
-  // if (last_claim)
-  //   miner.ui.claimnfts.textContent = "claim nfts" + ` (last ${ms_to_time(now - last_claim)} ago)`;
-  try {
-    const unclaimed_nfts = await get_unclaimed_nfts();
-    const nft_count = unclaimed_nfts.rows.length;
-    if (nft_count > 0) {
-      miner.ui.claimnfts.textContent = "claim nfts" + ` (${nft_count} unclaimed nft !)`;
-      miner.ui.claimnfts.classList.remove("miner-hidden");
-    }
-    else {
-      miner.ui.claimnfts.textContent = "claim nfts (0)";
-      miner.ui.claimnfts.classList.add("miner-hidden");
-    }
-  }
-  catch (e) {
-    console.log(e);
-  }
-}
-
-// mining script below
-var BAG = [1, 2, 3];
-
-var my_set_bag = async function () {
-  var ACCOUNT = wax.userAccount;
-  bag_str = JSON.stringify({ items: BAG });
-  miner.print("SETTING BAG")
-  await server_setBag(ACCOUNT, bag_str);
-  miner.print("BAG SET")
 }
 
 var my_sleep = function (ms) {
@@ -339,230 +269,6 @@ var close_last_popup = function () {
   }
 }
 
-var test_popup_close = async function () {
-  monkey_patch_popup();
-  await try_close_popup();
-  console.log("POPUP CLOSED !")
-}
-
-var my_background_mine = async (account, timeout_ms) => {
-  let bagDifficulty = await getBagDifficulty(account);
-  bagDifficulty = parseInt(bagDifficulty) || 0; // [!]
-
-  let landDifficulty = await getLandDifficulty(account);
-  landDifficulty = parseInt(landDifficulty) || 0; // [!]
-
-  const difficulty = bagDifficulty + landDifficulty;
-  console.log('difficulty', difficulty);
-
-  console.log('start doWork = ' + Date.now());
-  const last_mine_tx = await lastMineTx(mining_account, account, wax.api.rpc);
-
-  return await doWorkWorker({ mining_account, account, difficulty, last_mine_tx });
-};
-
-var my_mine = async function (account, timeout_ms) {
-  try {
-    const mine_work = await my_background_mine(account, timeout_ms);
-    unityInstance.SendMessage('Controller', 'Server_Response_Mine', JSON.stringify(mine_work));
-    return JSON.stringify(mine_work);
-  } catch (error) {
-    unityInstance.SendMessage('ErrorHandler', 'Server_Response_SetErrorData', error.message);
-    throw error;
-  }
-}
-
-var my_claim = async function (data) {
-  return new Promise((resolve, reject) => {
-    var mine_work = JSON.parse(data);
-    try {
-      console.log(`${mine_work.account} Pushing mine results...`);
-      const mine_data = {
-        miner: mine_work.account,
-        nonce: mine_work.rand_str,
-      };
-      console.log('mine_data', mine_data);
-      const actions = [{
-        account: mining_account,
-        name: 'mine',
-        authorization: [{
-          actor: mine_work.account,
-          permission: 'active',
-        },],
-        data: mine_data,
-      },];
-      wax.api.transact({
-        actions,
-      }, {
-        blocksBehind: 3,
-        expireSeconds: 90,
-      }).then((result) => {
-        console.log('result is=', result);
-        var amounts = new Map();
-        if (result && result.processed) {
-          result.processed.action_traces[0].inline_traces.forEach((t) => {
-            if (t.act.data.quantity) {
-              const mine_amount = t.act.data.quantity;
-              console.log(`${mine_work.account} Mined ${mine_amount}`);
-              if (amounts.has(t.act.data.to)) {
-                var obStr = amounts.get(t.act.data.to);
-                obStr = obStr.substring(0, obStr.length - 4);
-                var nbStr = t.act.data.quantity;
-                nbStr = nbStr.substring(0, nbStr.length - 4);
-                var balance = (parseFloat(obStr) + parseFloat(nbStr)).toFixed(4);
-                amounts.set(t.act.data.to, balance.toString() + ' TLM');
-              } else {
-                amounts.set(t.act.data.to, t.act.data.quantity);
-              }
-            }
-          }
-          );
-          unityInstance.SendMessage('Controller', 'Server_Response_Claim', amounts.get(mine_work.account));
-          resolve();
-        }
-      }
-      ).catch((err) => {
-        unityInstance.SendMessage('ErrorHandler', 'Server_Response_SetErrorData', err.message);
-        reject(err);
-      }
-      );
-    } catch (error) {
-      unityInstance.SendMessage('ErrorHandler', 'Server_Response_SetErrorData', error.message);
-      reject(err);
-    }
-  });
-}
-
-async function my_getBalance(account) {
-  try {
-    var data = await getBalance(account, wax.api.rpc);
-    unityInstance.SendMessage('Controller', 'Server_Response_GetBalance', data);
-    let number = parseFloat(data);
-    return number;
-  } catch (error) {
-    unityInstance.SendMessage(
-      'ErrorHandler',
-      'Server_Response_SetErrorData',
-      error.message
-    );
-  }
-}
-
-async function my_getBag(account) {
-  try {
-    var data = await getBag(mining_account, account, wax.api.rpc, aa_api);
-    unityInstance.SendMessage(
-      'Controller',
-      'Server_Response_GetBag',
-      JSON.stringify(data)
-    );
-    return data;
-  } catch (error) {
-    unityInstance.SendMessage(
-      'ErrorHandler',
-      'Server_Response_SetErrorData',
-      error.message
-    );
-  }
-}
-
-async function retry_on_timeout(fn, do_when_timeount, timeout) {
-  while (true) {
-    try {
-      let result = await Promise.race([
-        fn(),
-        new Promise((res, rej) => setTimeout(rej.bind(this, "timeout"), timeout))
-      ]);
-
-      return result;
-    }
-    catch (err) {
-      if (err !== "timeout")
-        throw err;
-
-      do_when_timeount();
-      miner.print(err);
-      miner.print("RETRYING FUNCTION", fn.name);
-    }
-
-    await my_sleep(1 * 1000);
-  }
-}
-
-
-async function throw_on_timeout(fn, what, timeout) {
-  let result = await Promise.race([
-    fn(),
-    new Promise((res, rej) => setTimeout(rej.bind(this, what), timeout))
-  ]);
-
-  return result;
-}
-
-async function retry_on_reject(fn, reject_vallue) {
-  while (true) {
-    try {
-      const result = await fn();
-      return result;
-    }
-    catch (err) {
-      if (reject_vallue && err !== reject_vallue)
-        throw err;
-
-      miner.print(err);
-      miner.print("RETRYING FUNCTION", fn.name);
-    }
-    await my_sleep(1 * 1000);
-  }
-}
-
-const my_claimnft = async () => {
-  const actions = [{
-    account: mining_account,
-    name: 'claimnfts',
-    authorization: [{
-      actor: wax.userAccount,
-      permission: 'active',
-    }],
-    data: {
-      account: wax.userAccount,
-      miner: wax.userAccount
-    }
-  }];
-  const res = await wax.api.transact({
-    actions
-  }, {
-    blocksBehind: 3,
-    expireSeconds: 90,
-  });
-
-  return res;
-}
-
-async function get_unclaimed_nfts() {
-  return await wax.api.rpc.get_table_rows({
-    code: mining_account, scope: mining_account, table: 'claims', limit: 100,
-    lower_bound: wax.userAccount,
-    upper_bound: wax.userAccount
-  });
-}
-
-async function try_claimnft() {
-  const result = await my_claimnft();
-  //localStorage.setItem('last_nft_claim_date', Date.now());
-  await update_unclaimed_nft_count();
-}
-
-function do_every_nth_iteration(f, n) {
-  let iter = -1;
-  return async function () {
-    iter++;
-    if (iter % n == 0) {
-      return f();
-    }
-  }
-}
-
 function disable_unity_animation() {
   requestAnimationFrame = () => { };
 }
@@ -572,78 +278,70 @@ function change_wax_endpoint() {
   console.log("wax endpoint set to", wax.rpc.endpoint);
 }
 
+function elem_contains(selector, text) {
+  var elements = document.querySelectorAll(selector);
+  return Array.prototype.filter.call(elements, function(element){
+    return element.textContent === text;
+  });
+}
+
+function extract_wait_time() {
+  const text_elems = elem_contains('p', "Next Mine Attempt");
+  const time_str = text_elems.length ? text_elems[0].previousElementSibling.textContent : null;
+
+  let wait_ms = 0;
+  if (time_str)
+     wait_ms = Number(time_str.split(':')[0]) * 60 * 60 * 1000 + Number(time_str.split(':')[1]) * 60 * 1000 + Number(time_str.split(':')[2] * 1000);
+
+  return wait_ms;
+}
+
+function find_mine_button() {
+  const text_elems = elem_contains('span', "Mine");
+  const mine_button = text_elems.length ? text_elems[0].parentElement.parentElement : null;
+  return mine_button;
+}
+
+function find_claim_button() {
+  const text_elems = elem_contains('span', "Claim Mine");
+  const claim_mine = text_elems.length ? text_elems[0].parentElement.parentElement : null;
+  return claim_mine;
+}
+
 var my_mine_loop = async function () {
-  var ACCOUNT = wax.userAccount;
-  const NIGHT_MS = 7 * 60 * 60 * 1000;
-  const WORK_DAY_MS = 24 * 60 * 60 * 1000 - NIGHT_MS;
-  const ENABLE_SLEEP = true;
-
-  monkey_patch_popup();
-
   const elapsed = start_timer();
-  let awake = start_timer();
-  let start_balance = 0;
-  let mine_count = 0;
-  let cpu_fails = 0;
-
-  try { start_balance = await my_getBalance(ACCOUNT); }
-  catch { }
 
   const print_status = async () => {
     const uptime_ = uptime(elapsed());
-    const balance = await my_getBalance(ACCOUNT);
-    const earned = (balance - start_balance).toFixed(3);
-    const efficiency = (earned / elapsed() * 60 * 60 * 1000).toFixed(3); // TML/hour
-
     miner.ui.set_statusbar(
-      `balance: ${balance} TLM | total earned: ${earned} TLM | efficiency: ${efficiency} TLM/hour | uptime: ${uptime_} | mine count: ${mine_count} | cpu fails: ${cpu_fails}`
+      ` uptime: ${uptime_}`
     );
   }
 
   while (true) {
     try {
-      let delay = await getMineDelay(ACCOUNT);
+      const wait_ms = extract_wait_time();
 
-      if (delay > 0)
-        await countdown(delay, "wating for mine timeout");
+      if (wait_ms)
+        await countdown(wait_ms, "wating for mine timeout");
+      
+      const mine_button = find_mine_button();
+      if (mine_button) {
+        mine_button.click();
+        await countdown(3 * 1000, "mining");
+      }
 
-      miner.print("MINING");
-      mine_work = await throw_on_timeout(
-        my_mine.bind(this, ACCOUNT), "timeout on mining", 3 * 60 * 1000
-      );
-
-      await sleep_random(0, 2 * 60 * 1000); // sleep additional random secs
-
-      miner.print("CLAIMING RESULT");
-      await retry_on_timeout(my_claim.bind(this, mine_work), close_last_popup, 3 * 60 * 1000);
-
-      miner.print("DONE CLAIMING");
-
-      mine_count++;
+      const claim_button = find_claim_button();
+      if (claim_button) {
+        claim_button.click();
+        await countdown(3 * 1000, "claiming mine");
+      }
 
       print_status();
 
       await countdown(3 * 1000, "waiting a little"); // sleep additional secs
-
-      await update_unclaimed_nft_count();
-
-      if (ENABLE_SLEEP && awake() > WORK_DAY_MS) {
-        await countdown(NIGHT_MS, "ZzzZZzzZZzz..."); // sleeping in bed
-        miner.print("Waking up");
-        await sleep_random(0, 20 * 60 * 1000); // sleep additional random secs
-        awake = start_timer();
-      }
     }
     catch (error) {
-      const alien_error = error?.json?.error;
-      if (alien_error && error?.json?.error?.name === "tx_cpu_usage_exceeded") {
-        miner.print(alien_error?.what || alien_error);
-        cpu_fails++;
-        print_status();
-        await countdown(12 * 60 * 1000, "CPU cooldown");
-        continue;
-      }
-
       miner.print(error);
       await my_sleep(3 * 1000);
     }
@@ -651,12 +349,8 @@ var my_mine_loop = async function () {
 }
 
 var main = async function () {
-  // await wait_for_msg("Loaded HomeScene");
+  await wait_for_msg("Navigating to inventory");
   miner_init_ui();
-  disable_unity_animation();
-  await wait_for_msg("Input Manager initialize...");
-  change_wax_endpoint();
 }
 
-//miner_init_ui();
 main();
